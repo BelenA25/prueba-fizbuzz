a71d00de670ebee78f05e91dd06d59299accbf70
Belen <beckiar23@gmail.com>
2024-12-03 12:05:50 -0400

    Prueba de retorno del numero 2

--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -4,4 +4,9 @@ describe("Devolucion", () => {
     it("deberia retornar 1", () => {
       expect(fizzbuzz(1)).toEqual(1);
     });
+    it("deberia retornar el numero 2", () => {
+        expect(fizzbuzz(2)).toEqual(2);
+      });
   });
+
+  
